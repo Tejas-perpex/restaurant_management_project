1abb4476feb608123eb8202fff1e1f6eeee6a83e
Tejas-perpex <t21655635@gmail.com>
2025-08-06 21:39:15 +0530

    20129

--- account/models.py
+++ account/models.py
@@ -1,4 +1,7 @@
 from django.db import models
+from django.contrib.auth.models import User
+from products.models import MenuItem
+
 
 class MenuItem(models.Model):
     name = models.CharField(max_length = 100)
@@ -8,3 +11,19 @@ class MenuItem(models.Model):
     def __str__(self):
         return self.name
 
+class Order(models.Model):
+    STATUS_CHOICES = [
+        ('PENDING', 'Pending'),
+        ('CONFIRMED','Confirmed'),
+        ('DELIVERED', 'Delivered'),
+        ('CANCELLED', 'Cancelled')
+    ]
+
+    customer = models.ForiegnKeyUser(User , on_delete = models.CASCADE)
+    items = models.ManyToManyFields(MenuItem)
+    total_amount = models.DecimalField(max_digit = 10 , decimal_place =2)
+    status = models.CharField(max_length = 0 , choices = STATUS_CHOICES , default = 'PENDING')
+    created_at = models.DateTimeField(auto_now_add = True)
+
+    def __str__(self):
+        return f"Order #{self.id} - {self.customer.username}"
