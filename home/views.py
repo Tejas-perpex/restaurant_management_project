251ddfe20ae006948b50c74f3efd884c09a1ae69
Tejas-perpex <t21655635@gmail.com>
2025-08-06 21:52:01 +0530

    20131

--- home/views.py
+++ home/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from rest_framework import APIView
+from rest_framework import status
+from .models import MenuItem
+from .serializers import ItemSerializer
 
-# Create your views here.
+class MenuView(APIView):
+    def get(self, request):
+        items = Item.objects.all()
+        serializer = ItemSerializer(items , many = True)
+        return Response(serializer.data , status =status.HTTP_200_OK)
+
