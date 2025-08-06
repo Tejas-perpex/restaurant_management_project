b1871f0471c8cb86a66cdd8b3c460be6895d6975
Tejas-perpex <t21655635@gmail.com>
2025-08-06 21:19:50 +0530

    20127

--- products/serializers.py
+++ products/serializers.py
@@ -1,2 +1,7 @@
 from rest_framework import serializers
-from .models import Items
+from .models import Item
+
+class ItemSerializer(serializers.ModelSerializer):
+    class Meta
+    model = Item
+    fields = ['id','item_name','item_price','created_at']
