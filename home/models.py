6d82c04ad801a9cac03702f2cd06a61263c0b31b
Tejas-perpex <t21655635@gmail.com>
2025-08-06 21:02:44 +0530

    20127

--- home/models.py
+++ home/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class UserProfile(models.Model):
+    user = models.OneToOneField(User , on_delete = models.CASCADE)
+    name = models.CharField(max_length = 100)
+    email = models.EmailField()
+    phone_number = models.CharField(max_length = 15)
+
+    def__str__(self):
+        return self.name
+
