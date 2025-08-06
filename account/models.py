10ddd7b4683454902795786e983908f3a0d32677
Tejas-perpex <t21655635@gmail.com>
2025-08-06 21:30:22 +0530

    20128

--- account/models.py
+++ account/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
+class MenuItem(models.Model):
+    name = models.CharField(max_length = 100)
+    description = models.TextField()
+    price = models.DecimalField(max_digit = 6 , decimal_place = 2)
+
+    def __str__(self):
+        return self.name
 
