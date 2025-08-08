e088023dde685fab3f431138b750f2b4a21abcf8
Tejas-perpex <t21655635@gmail.com>
2025-08-08 20:58:21 +0530

    Update views.py

--- home/views.py
+++ home/views.py
@@ -1,12 +1,10 @@
+from django.conf import settings
 from django.shortcuts import render
-from rest_framework import APIView
-from rest_framework import status
-from .models import MenuItem
-from .serializers import ItemSerializer
 
-class MenuView(APIView):
-    def get(self, request):
-        items = Item.objects.all()
-        serializer = ItemSerializer(items , many = True)
-        return Response(serializer.data , status =status.HTTP_200_OK)
+def homepage(request):
+    context = {
+        'restaurant_name': settings.RESTAURANT_NAME
+    }
+    return render(request, 'home.html', context)
+
 
