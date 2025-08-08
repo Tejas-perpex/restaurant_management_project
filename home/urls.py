bc6aa5245b692b86ef90f9f915ed9cf97926d74b
Tejas-perpex <t21655635@gmail.com>
2025-08-08 21:02:10 +0530

    Update urls.py

--- home/urls.py
+++ home/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import homepage
+from .views import homepage, about
 
 urlpatterns = [
     path('', homepage, name='homepage'),
+    path('about/', about, name='about'),
 ]
+
