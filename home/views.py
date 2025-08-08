64bec715b23a2b1908d87380b380f4792c6e2541
Tejas-perpex <t21655635@gmail.com>
2025-08-08 21:15:12 +0530

    Update views.py

--- home/views.py
+++ home/views.py
@@ -1,9 +1,8 @@
-from django.conf import settings
 from django.shortcuts import render
 
 def homepage_view(request):
-    phone_number = settings.RESTAURANT_PHONE
-    return render(request, 'home.html', {'phone_number': phone_number})
+    return render(request, 'home.html', {'restaurant_name': 'Tasty Bites'})
+
 
 
 
