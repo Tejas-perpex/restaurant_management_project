901cd5328f59b876fc38367246b9c97b488eb2dd
Tejas-perpex <t21655635@gmail.com>
2025-08-08 21:09:06 +0530

    Update views.py

--- home/views.py
+++ home/views.py
@@ -1,12 +1,7 @@
 from django.shortcuts import render
 
-def menu_list(request):
-    # Hardcoded menu items for now
-    menu_items = [
-        {"name": "Margherita Pizza", "price": 8.99},
-        {"name": "Pasta Alfredo", "price": 10.50},
-        {"name": "Caesar Salad", "price": 6.75},
-    ]
-    return render(request, "menu.html", {"menu_items": menu_items})
+def contact_view(request):
+    return render(request, 'contact.html')
+
 
 
