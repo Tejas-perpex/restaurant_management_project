3722a172f7abcd2334ffc6469d6f993165d2a9d6
Tejas-perpex <t21655635@gmail.com>
2025-08-11 00:27:52 +0530

    Update urls.py

--- home/urls.py
+++ home/urls.py
@@ -2,7 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.homepage_view, name='home'),
+    path('reservations/', views.reservations_view, name='reservations'),
 ]
+
 
 
