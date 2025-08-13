44c9962a29c13d4c847b2ebaaa3a4a856590718a
Tejas-perpex <t21655635@gmail.com>
2025-08-14 01:00:39 +0530

    Create forms1.py

---
+++ home/forms1.py
@@ -0,0 +1,14 @@
+from django import forms
+from .models import Feedback
+
+class FeedbackForm(forms.ModelForm):
+    class Meta:
+        model = Feedback
+        fields = ['comment']
+        widgets = {
+            'comment': forms.Textarea(attrs={
+                'rows': 4,
+                'placeholder': 'Write your feedback here...',
+                'style': 'width:100%; padding:8px; border-radius:5px; border:1px solid #ccc;'
+            }),
+        }
