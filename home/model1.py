4ae393d69d596bc7aba8a941c9793cb619d41968
Tejas-perpex <t21655635@gmail.com>
2025-08-20 22:18:12 +0530

    Update model1.py

--- home/model1.py
+++ home/model1.py
@@ -1,8 +1,8 @@
 from django.db import models
 
-class Feedback(models.Model):
-    comment = models.TextField()
-    created_at = models.DateTimeField(auto_now_add=True)
+class ContactSubmission(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField()
 
     def __str__(self):
-        return f"Feedback #{self.id}"
+        return f"{self.name} - {self.email}"
