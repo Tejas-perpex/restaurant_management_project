5819ba98f22e9b8b6e640f9fb7d7517204b9b262
Tejas-perpex <t21655635@gmail.com>
2025-08-20 22:16:23 +0530

    34798

--- name.py
+++ name.py
@@ -4,7 +4,7 @@ class RestaurantLocation(models.Model):
     address = models.CharField(max_length=255)
     city = models.CharField(max_length=100)
     state = models.CharField(max_length=100)
-    zip_code = models.CharField(max_length=10)
+    zip_code = models.CharField(max_length = 100)
 
     def __str__(self):
         return f"{self.address}, {self.city}, {self.state} {self.zip_code}"
